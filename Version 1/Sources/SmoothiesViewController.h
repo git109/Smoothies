5c556f74e9119178460e69d0854683f7f440a135
Matthijs Hollemans <mail@hollance.com>
2011-04-07 09:17:14 +0200

    Initial commit.

---
+++ Version 1/Sources/SmoothiesViewController.h
@@ -0,0 +1,9 @@
+
+/*
+ * This is the view controller that lists all smoothie recipes.
+ */
+@interface SmoothiesViewController : UITableViewController
+{
+}
+
+@end
---
+++ Version 1/Sources/SmoothiesViewController.m
@@ -0,0 +1,11 @@
+
+#import "SmoothiesViewController.h"
+
+@implementation SmoothiesViewController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
