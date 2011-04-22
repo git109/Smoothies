cd25f9a65f645925d84a7333e5a51be21dfbcc04
Matthijs Hollemans <mail@hollance.com>
2011-04-22 14:06:45 +0200

    Added version 7. Uses notifications to communicate between view controllers.

---
+++ Version 7/Sources/DataModel.h
@@ -0,0 +1,40 @@
+
+@class Recipe;
+
+/*
+ * Notification that is sent when the list of favorites is changed.
+ */
+extern NSString* const FavoritesChangedNotification;
+
+/*
+ * The top-level data model object for our app.
+ */
+@interface DataModel : NSObject
+{
+}
+
+// Returns how many recipes are in the list.
+- (int)recipeCount;
+
+// Returns the Recipe object at the specified position in the list.
+- (Recipe*)recipeAtIndex:(int)index;
+
+// Deletes a Recipe object from the list.
+- (void)removeRecipeAtIndex:(int)index;
+
+// Returns the number of favorites.
+- (int)favoritesCount;
+
+// Returns the Recipe object at the specified position in the favorites list.
+- (Recipe*)favoriteAtIndex:(int)index;
+
+// Determines whether a Recipe is in the list of favorites.
+- (BOOL)isFavorite:(Recipe*)recipe;
+
+// Adds a Recipe to the favorites.
+- (void)addToFavorites:(Recipe*)recipe;
+
+// Removes a Recipe from the favorites.
+- (void)removeFromFavorites:(Recipe*)recipe;
+
+@end
---
+++ Version 7/Sources/FavoritesViewController.m
@@ -0,0 +1,85 @@
+
+#import "FavoritesViewController.h"
+#import "DataModel.h"
+#import "Recipe.h"
+
+@implementation FavoritesViewController
+
+@synthesize dataModel;
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+	// Set the height of the table view rows
+	self.tableView.rowHeight = 66;
+
+	// Start listening for notifications
+	[[NSNotificationCenter defaultCenter] addObserver:self
+		selector:@selector(favoritesChanged:)
+		name:FavoritesChangedNotification
+		object:nil];
+}
+
+- (void)viewDidUnload
+{
+	[super viewDidUnload];
+
+	// Stop listening for notifications
+	[[NSNotificationCenter defaultCenter] removeObserver:self
+		name:FavoritesChangedNotification
+		object:nil];
+}
+
+- (void)dealloc
+{
+	// Stop listening for notifications
+	[[NSNotificationCenter defaultCenter] removeObserver:self
+		name:FavoritesChangedNotification
+		object:nil];
+
+	[super dealloc];
+}
+
+- (void)favoritesChanged:(NSNotification*)notification
+{
+	[self.tableView reloadData];
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView*)theTableView numberOfRowsInSection:(NSInteger)section
+{
+	// Return the number of favorites
+	return [self.dataModel favoritesCount];
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
+{
+	static NSString* CellIdentifier = @"RecipeCellIdentifier";
+
+	// Obtain a table view cell for this row
+	UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (cell == nil)
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+
+	// Get the favorite Recipe from the list
+	Recipe* recipe = [self.dataModel favoriteAtIndex:indexPath.row]; 
+
+	// Put the Recipe data into the cell
+	cell.textLabel.text = recipe.name;
+	cell.imageView.image = recipe.image;
+
+	return cell;
+}
+
+#pragma mark -
+#pragma mark UITableViewDelegate
+
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
+{
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
